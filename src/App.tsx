fdc59b5cda498375888f1b5287d38303a12f4686
skies-codes <akashdebnathwd@gmail.com>
2024-06-30 15:08:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className='w-full h-screen flex items-center justify-center'>
+                <h1>Hello world!</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
